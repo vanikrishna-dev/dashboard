6ecd9c7b81b3ddabff4a6a9097f05f7c9f1a515f
vanikrishna-dev <vanikrishna02@gmail.com>
2026-09-08 16:10:37 +0530

    adding author name

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 	.then(data => {
 		console.log(data.urls.full)
 		document.body.style.backgroundImage=`url('${data.urls.full}')`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
 	})
