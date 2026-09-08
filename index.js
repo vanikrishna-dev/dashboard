fd2042f7803abbc8f976b0c8c24029d9c33d631d
vanikrishna-dev <vanikrishna02@gmail.com>
2026-09-08 16:04:02 +0530

    setting up background image

---
+++ index.js
@@ -0,0 +1,6 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+	.then(res => res.json())
+	.then(data => {
+		console.log(data.urls.full)
+		document.body.style.backgroundImage=`url('${data.urls.full}')`
+	})
