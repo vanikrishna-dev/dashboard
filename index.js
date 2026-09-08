b6fb53b0c44bb40fd828d2d279abb299169d661d
vanikrishna-dev <vanikrishna02@gmail.com>
2026-09-08 17:05:25 +0530

    adding crypto data

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-	.then(res => res.json())
-	.then(data => {
-		console.log(data.urls.full)
-		document.body.style.backgroundImage=`url('${data.urls.full}')`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-	})
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
+  })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
+  });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    document.getElementById("topSection").innerHTML =
+      `<img src=${data.image.thumb} />
+        <p>${data.name}</p>`;
+  })
+  .catch((err) => console.error(err));
