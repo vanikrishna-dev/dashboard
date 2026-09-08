2127e25cbf9491f2363c909d184a13d7ac320bc6
vanikrishna-dev <vanikrishna02@gmail.com>
2026-09-08 19:21:16 +0530

    add weather info

--- index.js
+++ index.js
@@ -40,5 +40,22 @@ setInterval(getCurrentTime, 1000)
 
 
 navigator.geolocation.getCurrentPosition(position => {
-    console.log(position)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+            const temp = Math.round(data.main.temp)
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p>${temp}</p>
+                <p>${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
 });
